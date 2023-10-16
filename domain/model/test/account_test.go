5e1660575e6361fab15a2a4b558e9a53c4f63610
joaoptgaino <joaoptgaino@hotmail.com>
2023-10-16 18:25:35 -0300

    aula-01

---
+++ domain/model/test/account_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/joaoptgaino/imersao-codepix-go/domain/model"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestModel_NewAccount(t *testing.T) {
+	code := "986"
+	name := "JoaoBank"
+	bank, err := model.NewBank(code, name)
+
+	accountNumber := "act_0001"
+	ownerName := "Joao Gaino"
+
+	account, err := model.NewAccount(bank, accountNumber, ownerName)
+
+	require.Nil(t, err)
+	require.NotNil(t, uuid.FromStringOrNil(account.ID))
+	require.Equal(t, account.Number, accountNumber)
+	require.Equal(t, account.Bank.ID, bank.ID)
+
+	_, err = model.NewAccount(bank, "", ownerName)
+	require.NotNil(t, err)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/joaoptgaino/imersao-codepix-go
+
+go 1.15
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+)
